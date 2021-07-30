19b4133f07bf78d91ccfb309a729a5acb150dcf2
Stanislau Turasau <pontty232@gmail.com>
2021-07-30 23:47:46 +0300

    fix: fix objects coercion examples

--- hometask02/coercion.js
+++ hometask02/coercion.js
@@ -213,71 +213,73 @@ obj.addEventListener('click', event => {
     for(item of convField) {
 
       if(item.querySelector('.objects_boolean_result')) {
-        item.querySelector('.objects_boolean_result').value = 'true + false  ==> 1 + 0  ==> 1';
+        item.querySelector('.objects_boolean_result').value = `${true + false}`;
       }
 
       if(item.querySelector('.objects_num_str_result')) {
-        item.querySelector('.objects_num_str_result').value = '12 / "6"  ==> 12 / 6  ==> 2';
+        item.querySelector('.objects_num_str_result').value = `${12 / '6'}`;
       }
 
       if(item.querySelector('.objects_str_num_num_result')) {
-        item.querySelector('.objects_str_num_num_result').value = '"number" + 15 + 3  ==> "number15" + 3  ==> "number153"';
+        item.querySelector('.objects_str_num_num_result').value = `"${'number' + 15 + 3}"`;
       }
 
       if(item.querySelector('.objects_num_num_str_result')) {
-        item.querySelector('.objects_num_num_str_result').value = '15 + 3 + "number"  ==> 18 + "number"  ==> "18number"';
+        item.querySelector('.objects_num_num_str_result').value = `"${15 + 3 + 'number'}"`;
       }
 
       if(item.querySelector('.objects_arr_null_result')) {
-        item.querySelector('.objects_arr_null_result').value = `[1] > null  ==> '1' > 0  ==> 1 > 0  ==> true`;
+        item.querySelector('.objects_arr_null_result').value = `${[1] > null}`;
       }
 
       if(item.querySelector('.objects_str_un_str_result')) {
-        item.querySelector('.objects_str_un_str_result').value = `"foo" + + "bar"  ==> "foo" + (+"bar")  ==> "foo" + NaN  ==> "fooNaN"`;
+        item.querySelector('.objects_str_un_str_result').value = `"${'foo' + + 'bar'}"`;
       }
 
       if(item.querySelector('.objects_str_bool_result')) {
-        item.querySelector('.objects_str_bool_result').value = `'true' == true  ==> NaN == 1  ==> false`;
+        item.querySelector('.objects_str_bool_result').value = `${'true' == true}`;
       }
 
       if(item.querySelector('.objects_bool_str_result')) {
-        item.querySelector('.objects_bool_str_result').value = `false == 'false'  ==> 0 == NaN  ==> false`;
+        item.querySelector('.objects_bool_str_result').value = `${false == 'false'}`;
       }
 
       if(item.querySelector('.objects_null_str_result')) {
-        item.querySelector('.objects_null_str_result').value = `null == ''  ==> false`;
+        item.querySelector('.objects_null_str_result').value = `${null == ''}`;
       }
 
       if(item.querySelector('.objects_log_bool_str_result')) {
-        item.querySelector('.objects_log_bool_str_result').value = `!!"false" == !!"true"  ==> true == true  ==> true`;
+        item.querySelector('.objects_log_bool_str_result').value = `${!!"false" == !!"true"}`;
       }
 
       if(item.querySelector('.objects_arr_str_result')) {
-        item.querySelector('.objects_arr_str_result').value = `['x'] == 'x'  ==> 'x' == 'x'  ==>  true`;
+        item.querySelector('.objects_arr_str_result').value = `${['x'] == 'x'}`;
       }
 
       if(item.querySelector('.objects_arr_null_num_result')) {
-        item.querySelector('.objects_arr_null_num_result').value = `[] + null + 1  ==>  '' + null + 1  ==>  'null' + 1  ==> 'null1'`;
+        item.querySelector('.objects_arr_null_num_result').value = `'${[] + null + 1}'`;
       }
 
       if(item.querySelector('.objects_arr_arr_result')) {
-        item.querySelector('.objects_arr_arr_result').value = `[1,2,3] == [1,2,3]  ==>  false`;
+        item.querySelector('.objects_arr_arr_result').value = `${[1,2,3] == [1,2,3]}`;
       }
 
       if(item.querySelector('.objects_mix_exp_result')) {
-        item.querySelector('.objects_mix_exp_result').value = `{}+[]+{}+[1]  ==> +[]+{}+[1]  ==> 0 + {} + [1]  ==> 0 + '[object Object]' + [1]  ==> '0[object Object]' + [1]  ==> '0[object Object]' + '1'  ==> '0[object Object]1'`;
+        //some tricks for correct result of the example
+        //do not repeat this at home
+        item.querySelector('.objects_mix_exp_result').value = `'${ + [] + {} + [1]}'`;
       }
 
       if(item.querySelector('.objects_mix_log_exp_result')) {
-        item.querySelector('.objects_mix_log_exp_result').value = `!+[]+[]+![]  ==> (!+[]) + [] + (![])  ==> !0 + [] + false  ==> true + [] + false  ==> true + '' + false  ==> 'truefalse'`;
+        item.querySelector('.objects_mix_log_exp_result').value = `'${!+[]+[]+![]}'`;
       }
 
       if(item.querySelector('.objects_date_minus_result')) {
-        item.querySelector('.objects_date_minus_result').value = `new Date(0) - 0  ==> 0 - 0  ==> 0`;
+        item.querySelector('.objects_date_minus_result').value = `${new Date(0) - 0}`;
       }
 
       if(item.querySelector('.objects_date_plus_result')) {
-        item.querySelector('.objects_date_plus_result').value = `new Date(0) + 0  ==> 'Thu Jan 01 1970 02:00:00 GMT+0200 (EET)' + 0  ==> 'Thu Jan 01 1970 02:00:00 GMT+0200 (EET)0'`;
+        item.querySelector('.objects_date_plus_result').value = `'${new Date(0) + 0}'`;
       }
     }
   } else {
